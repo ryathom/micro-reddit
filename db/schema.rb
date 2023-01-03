8dca68a5d123978b0a30fa0a233c129cfcd34368
Ryan Thomas <ryanthomasj15@gmail.com>
2023-01-03 19:14:57 +0000

    Add Post model and validations, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_185411) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_190729) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
